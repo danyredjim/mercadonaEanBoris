8df30568a3454879f594cd6332d1d92b9ad7e5d5
drjimenez <danredjim@gmail.com>
2025-11-13 15:10:49 +0100

    Initial commit

---
+++ src/main/java/com/example/mercadonaEanBoris/MercadonaEanBorisApplication.java
@@ -0,0 +1,13 @@
+package com.example.mercadonaEanBoris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadonaEanBorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadonaEanBorisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mercadonaEanBoris/MercadonaEanBorisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mercadonaEanBoris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadonaEanBorisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
